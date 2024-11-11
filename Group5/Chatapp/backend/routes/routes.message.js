f501f41f67a1e66544f1d407bfbb0826387053bf
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-11-12 00:38:08 +0530

    Update routes.message.js

--- Group5/Chatapp/backend/routes/routes.message.js
+++ Group5/Chatapp/backend/routes/routes.message.js
@@ -1,5 +1,6 @@
 import express from "express"
-import sendMessage from '../controllers/message.controllers.js'
+import sendMessage from "../controllers/message.controllers.js"
+
 const router = express.Router()
 router.post('/send',sendMessage)
 export default router
