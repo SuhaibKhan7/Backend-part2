6ea91611ad997ef7e8ac85544306658100075f4a
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-11-12 19:51:18 +0530

    class mid push

--- Group28/ChatApp/Backend/Controller/auth.controller.js
+++ Group28/ChatApp/Backend/Controller/auth.controller.js
@@ -1,88 +1,79 @@
-import User from "../model/auth.models.js"
-import bcrypt from "bcrypt"
+import User from "../model/auth.models.js";
+import generateJWT from "../utilities/generatejwt_cookies.js";
+
+import bcrypt from "bcrypt";
 export const singup = async (req, res) => {
+  try {
+    const { fullname, username, email, password, confirmpassword, gender } =
+      req.body;
+    // console.log(req.body)
+    if (password != confirmpassword) {
+      return res.status(400).send({ message: "Password mismatch....." });
+    }
+    const user = await User.findOne({ username });
 
-    try {
-        const { fullname, username, email, password, confirmpassword, gender } = req.body
-        // console.log(req.body)
-        if (password != confirmpassword) {
-            return res.status(400).send({ message: "Password mismatch....." })
-        }
-        const user = await User.findOne({ username })
-
-        if (user) {
-            console.log(user)
-            return res.status(400).send({ message: "User already Exist" })
-        }
-
-        const hashpassword = await bcrypt.hash(password, 10)
-
-
-        const boypic = `https://avatar.iran.liara.run/public/boy?username=${username}`
-        const girlpic = `https://avatar.iran.liara.run/public/girl?username=${username}`
-
-
-        const newuser = new User({
-            fullname,
-            username,
-            email,
-            password: hashpassword,
-            gender,
-            profilepic: gender == "male" ? boypic : girlpic
-        })
-
-        await newuser.save()
-
-        res.status(201).json({
-            _id: newuser._id,
-            fullname: newuser.fullname,
-            username: newuser.username,
-            email: newuser.email,
-            gender: newuser.gender,
-            profilepic: newuser.profilepic
-        })
-
-
-
-
-    } catch (error) {
-        res.status(500).send("Internal Server Error", error.message)
-
+    if (user) {
+      console.log(user);
+      return res.status(400).send({ message: "User already Exist" });
     }
 
+    const hashpassword = await bcrypt.hash(password, 10);
 
+    const boypic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlpic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
+    const newuser = new User({
+      fullname,
+      username,
+      email,
+      password: hashpassword,
+      gender,
+      profilepic: gender == "male" ? boypic : girlpic,
+    });
 
+    await newuser.save();
 
-}
-export const login = async (req, res) => {
-
-    const { username, password } = req.body
-
-    const user = await User.findOne({ username })
-
-
-    const isvalidpassword = await bcrypt.compare(password, user ? user.password : " ")
-
-    if (!user || !isvalidpassword) {
-
-        return res.status(401).json({ message: "Invalid username or password" })
-    }
-
-
-    
     res.status(201).json({
-        _id: user._id,
-        fullname: user.fullname,
-        username: user.username,
-        email: user.email,
-        gender: user.gender,
-        profilepic: user.profilepic
-    })
+      _id: newuser._id,
+      fullname: newuser.fullname,
+      username: newuser.username,
+      email: newuser.email,
+      gender: newuser.gender,
+      profilepic: newuser.profilepic,
+    });
+  } catch (error) {
+    res.status(500).send("Internal Server Error", error.message);
+  }
+};
+export const login = async (req, res) => {
+  const { username, password } = req.body;
 
+  const user = await User.findOne({ username });
 
+  const isvalidpassword = await bcrypt.compare(
+    password,
+    user ? user.password : " "
+  );
 
-}
+  if (!user || !isvalidpassword) {
+    return res.status(401).json({ message: "Invalid username or password" });
+  }
+
+  generateJWT(user._id, res);
+
+  res.status(201).json({
+    _id: user._id,
+    fullname: user.fullname,
+    username: user.username,
+    email: user.email,
+    gender: user.gender,
+    profilepic: user.profilepic,
+  });
+};
+
 export const logout = (req, res) => {
-
-}
+  res.cookie("jwt", " ", {
+    maxAge: 0,
+  });
+  res.status(200).send({ message: "Logout Successfully" });
+};
--- Group28/ChatApp/Backend/utilities/generatejwt_cookies.js
+++ Group28/ChatApp/Backend/utilities/generatejwt_cookies.js
@@ -1,15 +1,16 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 const generateJWT = (userid, res) => {
-    const token = jwt.sign({ userid }, process.env.JWT_SECRET_KEY, {
-        expiresIn: "1d"
-    })
-    
-    res.cookie("jwt", token, {
-        maxAge: 1 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-        secure: false
-    })
+  const token = jwt.sign({ userid }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "1d",
+  });
 
-    console.log("JWT", token);
+  res.cookie("jwt", token, {
+    maxAge: 1 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false,
+  });
 
-}
+  console.log("JWT", token);
+};
+
+export default generateJWT;
