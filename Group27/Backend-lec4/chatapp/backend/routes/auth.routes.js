1922941bb861db6446b816443b7d61e07b574106
SuhaibKhan7 <johnxuhaib@gmail.com>
2024-11-12 17:58:25 +0530

    Chat under lec4 deleted

--- Group27/Backend-lec4/chatapp/backend/routes/auth.routes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import { logout,login, signup } from '../controllers/auth.controller.js'
-const router = express.Router()
-router.post('/signup',signup)//function)
-router.post('/login',login)//function)
-router.post('/logout',logout)//function)
-export default router;
